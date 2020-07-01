334ca9de9d39805ba0e07cae64be1e4e70988a04
Andry :D <57609573+Nessa512@users.noreply.github.com>
2020-06-30 23:33:53 -0300

    Add files via upload

---
+++ cuidese.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="cuidese\cuidese.css">
+  <link rel="stylesheet" href="cuidese\bootstrap.css">
+  <title>Cuide-se</title>
+</head>
+
+
+<body>
+  
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="eaemulher.php">Eae mulher!</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+        <a class="nav-link" href="eaemulher.php">Menu </a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="cuidese.php">Cuide-se<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="blog.php">Fala!</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="sobre.php">Sobre</a>
+       </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Pesquise">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Pesquisar</button>
+      </form>
+    </div>
+    </nav>  
+  </header>
+
+  
+  <main>
+    
+    <center>
+      As dicas vão te dizer o que fazer mas só você decide o que você faz!
+    </center> <br><br><br><br><br> 
+
+    <div class="poshat">
+      <table>
+        <tr>
+          <th>
+            <div class="cartao" style="max-width: 20rem; color:black;">
+          <div class="header">Dics</div>
+            <div class="card-body">
+              <h4 class="card-title">Água!</h4>
+             <p class="card-text">Beba até 2L de água pordia para a boa manutenção do corpo! E como refresca! Se hidrate!! <br> </p>
+            </div>
+          </div>
+          </th>
+          <th>
+            <div class="cartao" style="max-width: 20rem; color:black;">
+            <div class="header">Dics</div>
+              <div class="card-body">
+              <h4 class="card-title">Já leu hoje?</h4>
+              <p class="card-text">A leitura ajuda a exercitar a concentração, raciocíonio, imaginação e interpretação.</p>
+            </div>
+          </div>
+          </th>
+          <th>
+          <div class="cartao" style="max-width: 20rem; color:black;">
+            <div class="header">Dics</div>
+              <div class="card-body">
+              <h4 class="card-title">Exercita!</h4>
+              <p class="card-text">Fazer exercícios ajuda você a ter uma melhor qualidade de vida! E ajuda em muita coisa hein</p>
+            </div>
+          </th>
+          <th>
+          <div class="cartao" style="max-width: 20rem; color:black;">
+            <div class="header">Dics</div>
+              <div class="card-body">
+              <h4 class="card-title">Sinceridade</h4>
+              <p class="card-text">Seja sincera consigo mesma! Com seus sentimentos e pensamentos.</p>
+            </div>
+          </th>
+        </tr>
+         </table>
+      </div>
+    
+  </main>
+
+  
+  
+  <footer>
+    <div class="animation">
+      <!-- Java: jQuery, Bootstrap and Popper -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+	    <!-- MathJax  -->
+	    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	    <script id="MathJax-" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    </div>
+    <div class="contato">
+      <h4>Nos siga nas redes sociais</h4>
+      <a href="https://pt-br.facebook.com/facebook" target="_blank" >facebook</a>
+      <a href="https://www.google.com.br/" target="_blank" >google</a>
+      <a href="https://twitter.com/login?lang=pt" target="_blank">twitter</a>
+    </div>
+    <h6>Eae mulher &copy;</h6>
+  </footer>
+</body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="5">
+  <link rel="stylesheet" href="sobre\sobre.css">
+  <link rel="stylesheet" href="sobre\bootstrap.css">
+  <title>sobre</title>
+</head> 
+<body>
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="eaemulher.php">Eae mulher!</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="eaemulher.php">Menu<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cuidese.php">Cuide-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="blog.php">Fala!</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="sobre.php">Sobre</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Pesquise">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav>
+</header>
+
+<main>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis itaque dolore dignissimos, consequatur libero totam optio. Laudantium quo architecto porro at quaerat dolorem harum repellendus. Voluptatum iusto ipsum magni eveniet?
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, consequatur optio maiores suscipit esse repellat quasi, modi in et eligendi exercitationem non recusandae quia magnam earum repudiandae labore id doloribus.
+</main>
+
+<footer>
+<div class="animation">
+  <!-- Java: jQuery, Bootstrap and Popper -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+	<!-- MathJax  -->
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+</div>
+<div class="contato">
+    <h4>Nos siga nas redes sociais</h4>
+    <a href="https://pt-br.facebook.com/facebook" target="_blank" >facebook</a>
+    <a href="https://www.google.com.br/" target="_blank" >google</a>
+    <a href="https://twitter.com/login?lang=pt" target="_blank">twitter</a>
+  </div>
+  <h6>Eae mulher &copy;</h6>
+</footer>
+
+</body>
+
+</html>
